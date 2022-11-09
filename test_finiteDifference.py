d65993c2e484dddb29783ab5efc53528dcfa2119
Combi, A (Alessandro) <alessandro.combi@rabobank.com>
2022-11-09 18:04:34 +0100

    refactored finite difference bermudan tests

--- test_finiteDifference.py
+++ test_finiteDifference.py
@@ -4,7 +4,7 @@ import numpy as np
 import Payoffs as po
 import pandas as pd
 
-price_tolerance = 0.01
+price_tolerance = 0.05
 result_path = r"TestResults/Longstaff_results.csv"
 
 def finiteDifferenceBS_instance(sigma, rate, strike):
@@ -13,22 +13,23 @@ def finiteDifferenceBS_instance(sigma, rate, strike):
     spot = 1
     price_grid_size = 500
     time_grid_size = 5000
-    price_bs = bs.Call(rate, sigma, maturity, strike, spot)
-    fd = FiniteDifferenceBS(boundary, time_grid_size, price_grid_size)
     option = po.VanillaCall(strike, maturity, spot)
-    price = fd.price(option, sigma, rate)
-    assert np.abs(price - price_bs) / price_bs < price_tolerance
+    model = bs.BlackScholes(rate, sigma)
+    price_bs = model.Call(maturity, strike, spot)
+    fd = FiniteDifferenceBS(boundary, time_grid_size, price_grid_size)
+    price_fd = fd.price(option, sigma, rate)
+    assert np.abs(price_fd - price_bs) / price_bs < price_tolerance, f"failed test with strike={strike}, sigma={sigma}"
 
 def test_FiniteDifferenceBS():
-    sigma = [0.04*i for i in range(1, 10)]
+    sigma = np.linspace(0.04, 0.5, 10)
     rate = 0.4
-    strikes = [0.5 + i * 0.1 for i in range(1, 10)]
+    strikes = np.linspace(0.6, 1.5, 10)
 
     for vol in sigma:
         for strike in strikes:
             finiteDifferenceBS_instance(vol, rate, strike)
 
-def test_fd_full():
+def test_fd_bermudan():
     price_grid_size = 200
     time_grid_size = 5000
     exercise_dates = 50
@@ -42,10 +43,12 @@ def test_fd_full():
         maturity = single_result["maturity"]
         spot = single_result["spot"]
         sigma = single_result["sigma"]
-        fd_single(spot, sigma, strike, maturity, rate, price_grid_size, time_grid_size, single_result["fd american"], boundary, exercise_dates)
+        fd_single_bermudan(spot, sigma, strike, maturity, rate, price_grid_size, time_grid_size, single_result["fd american"], boundary, exercise_dates)
 
-def fd_single(spot, sigma, strike, maturity, rate, price_grid_size, time_grid_size, result, boundary, exercise_dates):
+def fd_single_bermudan(spot, sigma, strike, maturity, rate, price_grid_size, time_grid_size, result, boundary, exercise_dates):
     fd = FiniteDifferenceBS(boundary, time_grid_size, price_grid_size)
     option = po.VanillaPut(strike, maturity, spot)
     price = fd.price_bermudan(exercise_dates, sigma, rate, option)
     assert np.abs(price - result) / result < 0.01
+
+test_FiniteDifferenceBS()
