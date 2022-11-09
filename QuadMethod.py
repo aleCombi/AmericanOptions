d842a68745ff5244c356bbecdcb61b21ce9e1440
Combi, A (Alessandro) <alessandro.combi@rabobank.com>
2022-11-09 18:03:45 +0100

    Added quad method for bermudan options

---
+++ QuadMethod.py
@@ -0,0 +1,33 @@
+import Payoffs as po
+import BlackScholes as bs
+import numpy as np
+import matplotlib.pyplot as plt
+import LSM 
+import FiniteDifference as fd
+
+class Quad:
+    def __init__(self, boundary, step, exercise_dates):
+        self.boundary = boundary
+        self.step = step
+        self.exercise_dates = exercise_dates
+    
+    def price_bermudan(self, option, bs_model):
+        exercise_dates = self.exercise_dates
+        k = option.maturity / exercise_dates
+        log_asset = np.arange(-self.boundary, self.boundary + self.step, self.step)
+        log_asset_vector = np.vstack(log_asset).T
+        log_asset_matrix = log_asset_vector-log_asset_vector.T
+        V = np.vstack(option.payoff(option.spot * np.exp(log_asset_vector))).T
+        F = bs_model.Density(log_asset_matrix, k)
+        f1 = np.vstack(F[:,0])
+        fN = np.vstack(F[:,-1])
+
+        for time in range(exercise_dates):
+            v1 = np.vstack(V[:,0])
+            vN = np.vstack(V[:,-1])
+            continuation = np.exp(- bs_model.r * k) * self.step * (F.dot(V) - 0.5 * (v1 * f1 + vN * fN))
+            exercise = option.payoff(option.spot * np.exp(log_asset))
+            u = np.maximum(continuation, np.vstack(exercise))
+            V = np.vstack(u)
+
+        return u[u.shape[0] // 2][0]
