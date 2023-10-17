f9f22859c7d00885730c3e7ff399a784e948ff3a
Combi, A (Alessandro) <alessandro.combi@rabobank.com>
2023-10-17 18:05:55 +0200

    Added term structure object for volatilities

--- TermStructure.py
+++ TermStructure.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.interpolate import interp1d
+from scipy.integrate import quad
 
 class TermStructure:
     def __init__(self, times, values):
@@ -6,6 +8,16 @@ class TermStructure:
             raise ValueError('A term structure must have the same number of times and values.')
         self.times = times
         self.values = values
+        self.interpolator = interp1d(times, values, kind="linear")
 
     def value(self, time):
-        return np.interp(time, self.times, self.values)
+        return self.interpolator(time)
+
+    def integral(self, start, end):
+        return quad(self.interpolator, start, end)
+
+    def square_integral(self, start, end):
+        return quad(lambda x: self.interpolator(x)**2, start, end)
+
+    def square_mean(self, start, end):
+        return np.sqrt(self.square_integral(start, end) / (end - start))
