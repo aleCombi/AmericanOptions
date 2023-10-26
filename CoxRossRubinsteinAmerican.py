59bb2c06c70cfbe42b293d41d5fa3e1ac4d62a6a
Combi, A (Alessandro) <alessandro.combi@rabobank.com>
2023-10-26 08:44:38 +0200

    Added draft Cox Ross Rubinstein implementation

--- CoxRossRubinsteinAmerican.py
+++ CoxRossRubinsteinAmerican.py
@@ -6,6 +6,18 @@ from scipy.stats import norm
 from TermStructure import TermStructure
 
 class CoxRossRubinsteinAmerican:
-    def __init__(self, r, sigma):
+    def __init__(self, num_steps, down_step, up_step, r):
+        self.num_steps = num_steps
+        self.down_step = down_step
+        self.up_step = up_step
         self.r = r
-        self.sigma = sigma
+
+    def PriceBermudan(self, option, bs_model):
+        steps = np.arange(self.num_steps)
+        strike = option.strike
+        spot = option.spot
+        u = self.up_step
+        d = self.down_step
+        r = self.r
+        p = (u - 1 - r) / (u - d)
+        price_at_maturity = np.maximum(strike - spot * )
